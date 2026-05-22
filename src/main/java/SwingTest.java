8825a3a816fc376297e356706f7777b0dd6fdb13
AutmanEA <autman.elatmioui@gmail.com>
2026-05-22 13:02:34 +0200

    feat: maven config with test file

---
+++ src/main/java/SwingTest.java
@@ -0,0 +1,32 @@
+package src.main.java;
+// Java program using label (swing)
+// to display the message “GFG WEB Site Click”
+import javax.swing.*;
+
+class Main {
+
+	// Main driver method
+	public static void main(String[] args)
+	{
+		// Creating instance of JFrame
+		JFrame frame = new JFrame();
+
+		// Creating instance of JButton
+		JButton button = new JButton(" GFG WebSite Click");
+
+		// x axis, y axis, width, height
+		button.setBounds(150, 200, 220, 50);
+
+		// adding button in JFrame
+		frame.add(button);
+
+		// 400 width and 500 height
+		frame.setSize(500, 600);
+
+		// using no layout managers
+		frame.setLayout(null);
+
+		// making the frame visible
+		frame.setVisible(true);
+	}
+}
